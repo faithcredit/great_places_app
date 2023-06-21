9d1fce32037bcef020589b49b154a9a7aea38b83
golangdevm <honest.dedicate@gmail.com>
2023-06-20 21:15:25 -0700

    Storing and Fetching data with SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -2,9 +2,10 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helper.dart';
 
 class GreatPlaces with ChangeNotifier {
-  final List<Place> _items = [];
+  List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
@@ -20,6 +21,27 @@ class GreatPlaces with ChangeNotifier {
         title: pickedTitle,
         location: PlaceLocation(latitude: 0, longitude: 0));
     _items.add(newPlace);
+    notifyListeners();
+
+    DBHelper.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: PlaceLocation(latitude: 0, longitude: 0),
+          ),
+        )
+        .toList();
     notifyListeners();
   }
 }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -19,26 +19,34 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        builder: (ctx, greatPlaces,ch) => greatPlaces.items.isEmpty
-            ? const Center(
-                child: Text('Got no places yet, start adding some!'),
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false).fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
               )
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(
-                      greatPlaces.items[i].image,
-                    ),
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    // Go to detail page ...
-                  },
-                ),
+            : Consumer<GreatPlaces>(
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.isEmpty
+                    ? const Center(
+                        child: Text('Got no places yet, start adding some!'),
+                      )
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[i].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
+                        ),
+                      ),
+                //
               ),
-        //      
       ),
     );
   }
